7e06eb33ad5b15fe0a10c29a33dc8f97f24f249a
streamlet <streamlet@outlook.com>
2019-03-22 16:45:40 +0800

    init

---
+++ CEFHelloWorld/Main.cpp
@@ -0,0 +1,31 @@
+// CEFHelloWorld.cpp : Defines the entry point for the application.
+//
+#include "stdafx.h"
+#include "MainWindow.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
+
+    if (!CreateMainWindow(hInstance))
+    {
+        return FALSE;
+    }
+
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, NULL, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
---
+++ CEFHelloWorld/MainWindow.h
@@ -0,0 +1,3 @@
+#pragma once
+
+bool CreateMainWindow(HINSTANCE hInstance);
